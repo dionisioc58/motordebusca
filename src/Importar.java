8dd37115a9dde2626f5b93c9a731a3a541be9e76
dionisioc58 <dionisioc58@gmail.com>
2017-11-27 22:40:36 -0300

    Correção import

--- src/Importar.java
+++ src/Importar.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
-import java.util.regex.Pattern;
 
 public class Importar {
 	
